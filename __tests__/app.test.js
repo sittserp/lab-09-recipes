d01f9d70d5092ceca37bbf90c5d4ed86a9ea30d6
Perry Sittser <sittserp@gmail.com>
2020-12-15 21:42:10 -0800

    Log tests passing too

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
 const Recipe = require('../lib/models/recipe');
+const Log = require('../lib/models/Log');
 
 describe('recipe-lab routes', () => {
   beforeEach(() => {
@@ -127,6 +128,150 @@ describe('recipe-lab routes', () => {
             'put dough on cookie sheet',
             'bake for 10 minutes'
           ]
+        });
+      });
+  });
+
+  it('creates a log', () => {
+    return request(app)
+      .post('/api/v1/logs')
+      .send({
+        recipeId: '1',
+        dateOfEvent: 'Dec. 15, 2020',
+        notes: 'This was a good batch.',
+        rating: '5 stars'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          recipeId: '1',
+          dateOfEvent: 'Dec. 15, 2020',
+          notes: 'This was a good batch.',
+          rating: '5 stars'
+        });
+      });
+  });
+
+  it('gets all logs', async() => {
+    await Recipe.insert({
+      name: 'cookies',
+      directions: [
+        'preheat oven to 375',
+        'mix ingredients',
+        'put dough on cookie sheet',
+        'bake for 10 minutes'
+      ]
+    });
+    const logs = await Promise.all([
+      {
+        recipeId: 1,
+        dateOfEvent: 'Dec. 15, 2020',
+        notes: 'This was a good batch.',
+        rating: '5 stars',
+      },
+      {
+        recipeId: 1,
+        dateOfEvent: 'Dec. 15, 2020',
+        notes: 'This was a good batch.',
+        rating: '5 stars',
+      },
+      {
+        recipeId: 1,
+        dateOfEvent: 'Dec. 15, 2020',
+        notes: 'This was a good batch.',
+        rating: '5 stars',
+      }
+    ].map(log => Log.insert(log)));
+
+    return request(app)
+      .get('/api/v1/logs')
+      .then(res => {
+        logs.forEach(log => {
+          expect(res.body).toContainEqual(log);
+        });
+      });
+  });
+
+  it('gets a log by recipeId', async() => {
+    const recipe = await Recipe.insert({
+      name: 'cookies',
+      directions: [
+        'preheat oven to 375',
+        'mix ingredients',
+        'put dough on cookie sheet',
+        'bake for 10 minutes'
+      ]
+    });
+    const log = await Log.insert({
+      recipeId: '1',
+      dateOfEvent: 'Dec. 15, 2020',
+      notes: 'This was a good batch.',
+      rating: '5 stars'
+    });
+    return request(app)
+      .get(`/api/v1/logs/${recipe.id}`)
+      .then(res => {
+        expect(res.body).toEqual(log);
+      });
+  });
+
+  it('updates a log by id', async() => {
+    const recipe = await Recipe.insert({
+      name: 'cookies',
+      directions: [
+        'preheat oven to 375',
+        'mix ingredients',
+        'put dough on cookie sheet',
+        'bake for 10 minutes'
+      ]
+    });
+    await Log.insert({
+      recipeId: 1,
+      dateOfEvent: 'Dec. 15, 2020',
+      notes: 'This was a good batch.',
+      rating: '5 stars'
+    });
+    return request(app)
+      .put(`/api/v1/logs/${recipe.id}`)
+      .send({
+        recipeId: 1,
+        dateOfEvent: 'Dec. 15, 2020',
+        notes: 'This was no good.',
+        rating: '1 star'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          recipeId: '1',
+          dateOfEvent: 'Dec. 15, 2020',
+          notes: 'This was no good.',
+          rating: '1 star'
+        });
+      });
+  });
+
+  it('deletes a log by recipeId', async() => {
+    const recipe = await Recipe.insert({
+      name: 'cookies',
+      directions: [
+        'preheat oven to 375',
+        'mix ingredients',
+        'put dough on cookie sheet',
+        'bake for 10 minutes'
+      ]
+    });
+    await Log.insert({
+      recipeId: '1',
+      dateOfEvent: 'Dec. 15, 2020',
+      notes: 'This was a good batch.',
+      rating: '5 stars'
+    });
+    return request(app)
+      .delete(`/api/v1/logs/${recipe.id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          recipeId: '1',
+          dateOfEvent: 'Dec. 15, 2020',
+          notes: 'This was a good batch.',
+          rating: '5 stars'
         });
       });
   });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,7 +1,15 @@
 DROP TABLE IF EXISTS recipes;
+DROP TABLE IF EXISTS logs;
 
 CREATE TABLE recipes (
   id BIGINT GENERATED ALWAYS AS IDENTITY,
   name TEXT NOT NULL,
   directions TEXT[]
+);
+
+CREATE TABLE logs (
+  recipe_id BIGINT,
+  date_of_event TEXT,
+  notes TEXT,
+  rating TEXT
 );
